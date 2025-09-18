118fc178a47b9928e823e96d8bb21fcb190771e7
devMarino <marcelomcb24@gmail.com>
2025-09-18 20:48:03 -0300

    Lista de Tarefa

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@ let tarefas = []
 
 const campoTarefa = document.querySelector('#campoTarefa');
 const addTarefa = document.querySelector('#addTarefa');
-const listaTarefas = document.querySelector('#listaTarefas')
+const listaTarefas = document.querySelector('#lista-tarefas');
 
-addTarefa.addEventListener('click', (e) => {
+    addTarefa.addEventListener('click', (e) => {
     e.preventDefault();
 
     const textoTarefa = campoTarefa.value.trim();
@@ -12,8 +12,24 @@ addTarefa.addEventListener('click', (e) => {
         tarefas.push({text: textoTarefa, concluido: false});
         alert("Tarefa adicionada com sucesso!")
         campoTarefa.value = '';
-        console.log(tarefas);
+        mostrarTarefas();
+    }
+    })
+
+    function mostrarTarefas() {
+        listaTarefas.innerHTML = '';
+        tarefas.forEach((tarefa, index) =>  {
+            const li = document.createElement('li');
+            li.classList.toggle('completo', tarefa.concluido);
+            li.innerHTML = `
+            <span>${tarefa.next}</span>
+            <input type = "checkbox" ${tarefa.concluido ? 'checked' : ''} 
+            onClick = "tarefaConcluida(${index})">
+            <button class = "editar" onclick = "editarTarefa(${index})>Editar</button>
+            <button class = "excluir" onclick="ExcluirTarefa(${index})>Excluir</button>
+            `
+            listaTarefas.appendChild(li);
         
 
+        });
     }
-})
